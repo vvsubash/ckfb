41246b40681a14342c646ece02d49bea73f158c1
venkat <subash.venkat007@gmail.com>
2020-10-21 03:35:39 +0530

    hhh

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-btn @click="logIn">{{ $fireAuth.currentUser || 'hi' }}</v-btn>
+  </div>
+</template>
+
+<script>
+import * as firebase from 'firebase/app'
+export default {
+  methods: {
+    async logIn() {
+      try {
+        const authProvider = new firebase.auth.GoogleAuthProvider()
+
+        await this.$fireAuth
+          .signInWithRedirect(authProvider)
+          .then((result) => {
+            alert(result.uid)
+          })
+          .catch((err) => {})
+      } catch (error) {
+        alert(error)
+      }
+    },
+  },
+}
+</script>
